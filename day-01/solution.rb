ad6062997189421744e9bf5409bc90d5756c9d28
Damien Gonot <damien.gonot@gmail.com>
2023-12-01 09:30:48 -0500

    day 1, part 2

--- day-01/solution.rb
+++ day-01/solution.rb
@@ -11,4 +11,31 @@ calibrations = file_array.map do |line|
 end
 
 # sum all calibrations
-puts calibrations.sum
+puts 'Part 1: ', calibrations.sum
+
+# part 2
+def word_to_digit(word)
+  words_to_digits = {
+    'one' => '1',
+    'two' => '2',
+    'three' => '3',
+    'four' => '4',
+    'five' => '5',
+    'six' => '6',
+    'seven' => '7',
+    'eight' => '8',
+    'nine' => '9'
+  }
+  words_to_digits[word]
+end
+
+calibrations2 = file_array.map do |line|
+  numbers = line.scan(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/).flatten
+
+  first_number = numbers.first.match?(/\d/) ? numbers.first : word_to_digit(numbers.first)
+  last_number = numbers.last.match?(/\d/) ? numbers.last : word_to_digit(numbers.last)
+
+  "#{first_number}#{last_number}".to_i
+end
+
+puts 'Part 2: ', calibrations2.sum
